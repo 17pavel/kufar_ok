edc3b0df8188ab1b78aba14025c2d619cfc97491
pasha <17pasha07@gmail.com>
2024-01-16 09:52:58 +0300

    controler and carousel

--- goods/views.py
+++ goods/views.py
@@ -17,7 +17,7 @@ def catalog(request, category_slug=None):
     elif query:
         goods = q_search(query)
     else:
-        goods = get_list_or_404(Products.objects.filter(category__slug=category_slug))
+        goods = get_list_or_404(Products.objects.prefetch_related("images").filter(category__slug=category_slug))
 
     if on_sale:
         goods = goods.filter(discount__gt=0)
